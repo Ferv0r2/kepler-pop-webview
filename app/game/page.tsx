817ed6d30a7ad700adb34a5abb9cdc9442567eb1
Ferv0r2 <amlk31255@gmail.com>
2025-04-15 00:27:52 +0900

    Fix: build error

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,5 +1,13 @@
+'use client';
+
+import { Suspense } from 'react';
+
 import { GameBoard } from '@/components/logic/GameBoard';
 
 export default function GameScreen() {
-  return <GameBoard />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <GameBoard />
+    </Suspense>
+  );
 }
