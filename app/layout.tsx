aa7e03da4100028b9c3162a9be2eca9c4adbfc53
Ferv0r2 <amlk31255@gmail.com>
2025-07-27 21:14:12 +0900

    feat: GTM 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import { Metadata, Viewport } from 'next';
 import { Press_Start_2P } from 'next/font/google';
 import localFont from 'next/font/local';
+import Script from 'next/script';
 import { ReactNode } from 'react';
 
 // import { PreloadDebugger } from '@/components/debug/PreloadDebugger';
@@ -242,6 +243,32 @@ export default function RootLayout({
         />
       </head>
       <body className={`${pixelMplusFont.variable} ${pressStart2P.variable} ${neodgmFont.variable} antialiased`}>
+        {/* Google Tag Manager */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WMZSQPNR');
+            `,
+          }}
+        />
+
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WMZSQPNR"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <div id="initial-loader" aria-busy="true" role="status" tabIndex={-1}>
           <StarsAndSparkles />
           <Logo className="mt-24" />
